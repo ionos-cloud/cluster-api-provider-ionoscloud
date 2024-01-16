05dad0fbaec07be608fc9a632e6b8c4240b47c3e
Ludwig Bedacht <ludwig.bedacht@ionos.com>
2024-01-16 11:36:57 +0100

    fix search replace

--- internal/controller/ionoscloudmachine_controller.go
+++ internal/controller/ionoscloudmachine_controller.go
@@ -57,7 +57,7 @@ type IonosCloudMachineReconciler struct {
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
-// the IonosMachine object against the actual cluster state, and then
+// the IonosCloudMachine object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
 //
