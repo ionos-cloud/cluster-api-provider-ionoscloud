ae244ecad176558142b64b9d05d4168ad5c3062d
Jonas Riedel <jonas.riedel@ionos.com>
2024-02-26 14:02:44 +0100

    rename `IsNullOrDefault` to `IsNilOrZero`

--- internal/util/ptr/ptr_test.go
+++ internal/util/ptr/ptr_test.go
@@ -42,7 +42,7 @@ func Test_PtrDeref(t *testing.T) {
 	require.Equal(t, Deref[testType](nil, testType{}), testType{})
 }
 
-func Test_IsNullOrDefault(t *testing.T) {
+func Test_IsNilOrZero(t *testing.T) {
 	// Test for a custom type
 	type testType struct {
 		value string
@@ -53,52 +53,52 @@ func Test_IsNullOrDefault(t *testing.T) {
 	}
 	// validate that nil is considered null or default
 	var nilTestType *testType
-	require.True(t, IsNullOrDefault(nilTestType))
-	require.True(t, IsNullOrDefault(emptyTestType))
+	require.True(t, IsNilOrZero(nilTestType))
+	require.True(t, IsNilOrZero(emptyTestType))
 	// validate that a non-nil value is not considered null or default
-	require.False(t, IsNullOrDefault(testTypeInstance))
+	require.False(t, IsNilOrZero(testTypeInstance))
 
 	// test all primitive types
 	var nilBool *bool
-	require.True(t, IsNullOrDefault(nilBool))
-	require.True(t, IsNullOrDefault(To(false)))
-	require.False(t, IsNullOrDefault(To(true)))
+	require.True(t, IsNilOrZero(nilBool))
+	require.True(t, IsNilOrZero(To(false)))
+	require.False(t, IsNilOrZero(To(true)))
 
 	var nilInt *int
-	require.True(t, IsNullOrDefault(nilInt))
-	require.True(t, IsNullOrDefault(To(0)))
-	require.False(t, IsNullOrDefault(To(1)))
+	require.True(t, IsNilOrZero(nilInt))
+	require.True(t, IsNilOrZero(To(0)))
+	require.False(t, IsNilOrZero(To(1)))
 
 	var nilUint *uint
-	require.True(t, IsNullOrDefault(nilUint))
-	require.True(t, IsNullOrDefault(To(uint(0))))
-	require.False(t, IsNullOrDefault(To(uint(1))))
+	require.True(t, IsNilOrZero(nilUint))
+	require.True(t, IsNilOrZero(To(uint(0))))
+	require.False(t, IsNilOrZero(To(uint(1))))
 
 	var nilFloat32 *float32
-	require.True(t, IsNullOrDefault(nilFloat32))
-	require.True(t, IsNullOrDefault(To(float32(0))))
-	require.False(t, IsNullOrDefault(To(float32(1))))
+	require.True(t, IsNilOrZero(nilFloat32))
+	require.True(t, IsNilOrZero(To(float32(0))))
+	require.False(t, IsNilOrZero(To(float32(1))))
 
 	var nilFloat64 *float64
-	require.True(t, IsNullOrDefault(nilFloat64))
-	require.True(t, IsNullOrDefault(To(float64(0))))
-	require.False(t, IsNullOrDefault(To(float64(1))))
+	require.True(t, IsNilOrZero(nilFloat64))
+	require.True(t, IsNilOrZero(To(float64(0))))
+	require.False(t, IsNilOrZero(To(float64(1))))
 
 	var nilString *string
-	require.True(t, IsNullOrDefault(nilString))
-	require.True(t, IsNullOrDefault(To("")))
-	require.False(t, IsNullOrDefault(To("test")))
+	require.True(t, IsNilOrZero(nilString))
+	require.True(t, IsNilOrZero(To("")))
+	require.False(t, IsNilOrZero(To("test")))
 
 	var nilByte *byte
-	require.True(t, IsNullOrDefault(nilByte))
-	require.True(t, IsNullOrDefault(To(byte(0))))
-	require.False(t, IsNullOrDefault(To(byte(1))))
+	require.True(t, IsNilOrZero(nilByte))
+	require.True(t, IsNilOrZero(To(byte(0))))
+	require.False(t, IsNilOrZero(To(byte(1))))
 
 	var nilInterface *interface{}
 	nilInterfaceTyped := (*any)(nil)
-	require.True(t, IsNullOrDefault(nilInterface))
-	require.True(t, IsNullOrDefault(nilInterfaceTyped))
-	require.True(t, IsNullOrDefault(To(any(nil))))
-	require.True(t, IsNullOrDefault[any](nil))
-	require.False(t, IsNullOrDefault(To(any(1))))
+	require.True(t, IsNilOrZero(nilInterface))
+	require.True(t, IsNilOrZero(nilInterfaceTyped))
+	require.True(t, IsNilOrZero(To(any(nil))))
+	require.True(t, IsNilOrZero[any](nil))
+	require.False(t, IsNilOrZero(To(any(1))))
 }
