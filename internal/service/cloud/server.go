6c75ce998b0317fc157eb1053a27643dba9e1ddc
Ludwig Bedacht <ludwig.bedacht@ionos.com>
2024-04-08 13:21:41 +0200

    make sure CPU Family is null when empty

--- internal/service/cloud/server.go
+++ internal/service/cloud/server.go
@@ -301,9 +301,12 @@ func (s *Service) buildServerProperties(ms *scope.Machine, machineSpec *infrav1.
 	props := sdk.ServerProperties{
 		AvailabilityZone: ptr.To(machineSpec.AvailabilityZone.String()),
 		Cores:            &machineSpec.NumCores,
-		CpuFamily:        &machineSpec.CPUFamily,
 		Name:             ptr.To(s.serverName(ms.IonosMachine)),
 		Ram:              &machineSpec.MemoryMB,
+	}
+
+	if machineSpec.CPUFamily != "" {
+		props.CpuFamily = &machineSpec.CPUFamily
 	}
 
 	return props
