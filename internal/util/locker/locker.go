06bf4e99eb592902575b948c2ceedcff3aaefd30
Matthias Bastian <matthias.bastian@ionos.com>
2024-05-28 23:19:18 +0200

    Add `locker` package

---
+++ internal/util/locker/locker.go
@@ -0,0 +1,123 @@
+/*
+Package locker provides a mechanism for fine-grained locking.
+
+In contrast to a sync.Mutex, the user must provide a key when locking and unlocking.
+
+If a lock with a given key does not exist when Lock() is called, one is created.
+Lock references are automatically cleaned up if nothing is waiting for the lock anymore.
+
+Locking can be aborted on context cancellation.
+
+This package is inspired by https://github.com/moby/locker, but uses a buffered channel instead of a sync.Mutex to allow
+for context cancellation.
+Even though the standard library does not offer to cancel locking a mutex, golang.org/x/sync/semaphore does for its
+Acquire() method.
+*/
+package locker
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+// Locker offers thread-safe locking of entities represented by keys of type string.
+type Locker struct {
+	mu    sync.Mutex // protects the locks map
+	locks map[string]*lockWithCounter
+}
+
+// New returns a new Locker.
+func New() *Locker {
+	locker := &Locker{
+		locks: make(map[string]*lockWithCounter),
+	}
+	return locker
+}
+
+// lockWithCounter is used by Locker to represent a lock for a given key.
+type lockWithCounter struct {
+	// ch is a buffered channel of size 1 used as a mutex.
+	// In contrast to a sync.Mutex, accessing the channel can be aborted upon context cancellation.
+	ch chan struct{}
+	// waiters is the number of callers waiting to acquire the lock.
+	// This is Int32 instead of Uint32, so we can add -1 in dec().
+	waiters atomic.Int32
+}
+
+// inc increments the number of waiters.
+func (lwc *lockWithCounter) inc() {
+	lwc.waiters.Add(1)
+}
+
+// dec decrements the number of waiters.
+func (lwc *lockWithCounter) dec() {
+	lwc.waiters.Add(-1)
+}
+
+// count gets the current number of waiters.
+func (lwc *lockWithCounter) count() int32 {
+	return lwc.waiters.Load()
+}
+
+// Lock locks the given key. Returns after acquiring lock or when given context is done.
+func (l *Locker) Lock(ctx context.Context, key string) error {
+	l.mu.Lock()
+
+	select {
+	case <-ctx.Done():
+		// ctx becoming done has "happened before" acquiring the lock, whether it became done before the call began or
+		// while we were waiting for the mutex. We prefer to fail even if we could acquire the mutex without blocking.
+		l.mu.Unlock()
+		return ctx.Err()
+	default:
+	}
+
+	lwc, exists := l.locks[key]
+	if !exists {
+		lwc = &lockWithCounter{
+			ch: make(chan struct{}, 1),
+		}
+		l.locks[key] = lwc
+	}
+	// Increment the waiters while inside the mutex to make sure that the lock isn't deleted if Lock() and Unlock()
+	// are called concurrently.
+	lwc.inc()
+
+	l.mu.Unlock()
+
+	// Lock the key outside the mutex, so we don't block other operations.
+	select {
+	case lwc.ch <- struct{}{}:
+		// Lock acquired, so we can decrement the number of waiters for this lock.
+		lwc.dec()
+		return nil
+	case <-ctx.Done():
+		// Locking aborted, so we can decrement the number of waiters for this lock.
+		lwc.dec()
+		// If there are no more waiters, we can delete the lock.
+		l.mu.Lock()
+		if lwc.count() == 0 {
+			delete(l.locks, key)
+		}
+		l.mu.Unlock()
+
+		return ctx.Err()
+	}
+}
+
+// Unlock unlocks the given key. It panics if the key is not locked.
+func (l *Locker) Unlock(key string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	lwc, exists := l.locks[key]
+	if !exists {
+		panic("no such lock: " + key)
+	}
+	<-lwc.ch
+
+	// If there are no more waiters, we can delete the lock.
+	if lwc.count() == 0 {
+		delete(l.locks, key)
+	}
+}
---
+++ internal/util/locker/locker_test.go
@@ -0,0 +1,146 @@
+package locker
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func withTimeout(t *testing.T, f func()) {
+	t.Helper()
+	done := make(chan struct{})
+	go func() {
+		f()
+		close(done)
+	}()
+	select {
+	case <-time.After(1 * time.Second):
+		t.Fatal("timed out")
+	case <-done:
+	}
+}
+
+func TestNew(t *testing.T) {
+	locker := New()
+	require.NotNil(t, locker)
+	require.NotNil(t, locker.locks)
+}
+
+func TestLockWithCounter(t *testing.T) {
+	lwc := &lockWithCounter{}
+	require.EqualValues(t, 0, lwc.count())
+
+	lwc.inc()
+	require.EqualValues(t, 1, lwc.count())
+
+	lwc.dec()
+	require.EqualValues(t, 0, lwc.count())
+}
+
+func TestLockerLock(t *testing.T) {
+	l := New()
+	require.NoError(t, l.Lock(context.Background(), "test"))
+	lwc := l.locks["test"]
+
+	require.EqualValues(t, 0, lwc.count())
+
+	chDone := make(chan struct{})
+	go func(t *testing.T) {
+		assert.NoError(t, l.Lock(context.Background(), "test"))
+		close(chDone)
+	}(t)
+
+	chWaiting := make(chan struct{})
+	go func() {
+		for range time.Tick(1 * time.Millisecond) {
+			if lwc.count() == 1 {
+				close(chWaiting)
+				break
+			}
+		}
+	}()
+
+	withTimeout(t, func() {
+		<-chWaiting
+	})
+
+	select {
+	case <-chDone:
+		t.Fatal("lock should not have returned while it was still held")
+	default:
+	}
+
+	l.Unlock("test")
+
+	withTimeout(t, func() {
+		<-chDone
+	})
+
+	require.EqualValues(t, 0, lwc.count())
+}
+
+func TestLockerUnlock(t *testing.T) {
+	l := New()
+
+	require.NoError(t, l.Lock(context.Background(), "test"))
+	l.Unlock("test")
+
+	require.PanicsWithValue(t, "no such lock: test", func() {
+		l.Unlock("test")
+	})
+
+	withTimeout(t, func() {
+		require.NoError(t, l.Lock(context.Background(), "test"))
+	})
+}
+
+func TestLockerConcurrency(t *testing.T) {
+	l := New()
+
+	var wg sync.WaitGroup
+	for i := 0; i <= 10_000; i++ {
+		wg.Add(1)
+		go func(t *testing.T) {
+			assert.NoError(t, l.Lock(context.Background(), "test"))
+			// If there is a concurrency issue, it will very likely become visible here.
+			l.Unlock("test")
+			wg.Done()
+		}(t)
+	}
+
+	withTimeout(t, wg.Wait)
+
+	// Since everything has unlocked the map should be empty.
+	require.Empty(t, l.locks)
+}
+
+func TestLockerContextCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	l := New()
+
+	withTimeout(t, func() {
+		err := l.Lock(ctx, "test")
+		require.ErrorIs(t, context.Canceled, err)
+	})
+}
+
+func TestLockerContextDeadlineExceeded(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
+	defer cancel()
+
+	l := New()
+	require.NoError(t, l.Lock(ctx, "test"))
+
+	withTimeout(t, func() {
+		err := l.Lock(ctx, "test")
+		require.ErrorIs(t, context.DeadlineExceeded, err)
+	})
+
+	require.Empty(t, l.locks)
+}
