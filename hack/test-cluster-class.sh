a047ef6f18bfeb68aea3d6c09738d7e9efdf317f
Jan Jansen <jan.jansen@gdata.de>
2024-06-11 16:29:58 +0200

    fix review commands

--- hack/test-cluster-class.sh
+++ hack/test-cluster-class.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 
-rm ./templates/clusterclass-template-replaced.yaml
-sed 's/${CLUSTER_CLASS_NAME}/cluster-class/g' ./templates/clusterclass-template.yaml > ./templates/clusterclass-template-replaced.yaml
-sed -i 's/${NAMESPACE}/default/g' ./templates/clusterclass-template-replaced.yaml
+source templates/.env
 
-rm ./templates/cluster-template-topology-replaced.yaml
-sed 's/${CLUSTER_CLASS_NAME}/cluster-class/g' ./templates/cluster-template-topology.yaml > ./templates/cluster-template-topology-replaced.yaml
-sed -i 's/${NAMESPACE}/default/g' ./templates/cluster-template-topology-replaced.yaml
+clusterctl generate yaml --from templates/clusterclass-template.yaml > ./templates/clusterclass-template-replaced.yaml
+clusterctl generate yaml --from templates/cluster-template-topology.yaml > ./templates/cluster-template-topology-replaced.yaml
 
-echo 'replace envs in ./templates/cluster-template-topology-replaced.yaml'
-echo 'clusterctl alpha topology plan -f ./templates/clusterclass-template-replaced.yaml -f ./templates/cluster-template-topology-replaced.yaml -o output/'
+echo "validate"
+clusterctl alpha topology plan -f ./templates/clusterclass-template-replaced.yaml -f ./templates/cluster-template-topology-replaced.yaml -o output/
+
+# echo "apply"
+#kubectl apply -f ./templates/clusterclass-template-replaced.yaml
+#kubectl apply -f ./templates/luster-template-topology-replaced.yaml
