4459c47cc66d7984231dcc67587be1dee3c6aae0
Jan Jansen <jan.jansen@gdata.de>
2024-06-12 15:24:24 +0200

    improve script

--- hack/test-cluster-class.sh
+++ hack/test-cluster-class.sh
@@ -1,13 +1,26 @@
 #/bin/bash
 
-source templates/.env
+ROOT_DIR=$(cd $(realpath $(dirname $0))/.. && pwd)
 
-clusterctl generate yaml --from templates/clusterclass-template.yaml > ./templates/clusterclass-template-replaced.yaml
-clusterctl generate yaml --from templates/cluster-template-topology.yaml > ./templates/cluster-template-topology-replaced.yaml
+if [[ ! -d "./output" ]]; then
+    echo "create output directory"
+    mkdir "./output"
+fi
 
-echo "validate"
-clusterctl alpha topology plan -f ./templates/clusterclass-template-replaced.yaml -f ./templates/cluster-template-topology-replaced.yaml -o output/
+if [[ ! -f "./.envfile" ]]; then
+    echo "create .envfile from envfile.example"
+    exit 1
+fi
+source ./.envfile
+
+clusterctl generate yaml --from templates/clusterclass-template.yaml > ./output/generated-clusterclass-template-replaced.yaml
+clusterctl generate cluster $CLUSTER_NAME --from templates/cluster-template-topology.yaml > ./output/generated-cluster-template-topology.yaml
+#
+#echo "validate"
+
+echo "done"
+#clusterctl alpha topology plan -f ./templates/clusterclass-template-replaced.yaml -f ./templates/cluster-template-topology-replaced.yaml -o output/
 
 # echo "apply"
 #kubectl apply -f ./templates/clusterclass-template-replaced.yaml
-#kubectl apply -f ./templates/luster-template-topology-replaced.yaml
+#kubectl apply -f ./templates/cluster-template-topology-replaced.yaml
