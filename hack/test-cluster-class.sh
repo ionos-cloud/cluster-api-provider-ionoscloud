2010b20b867885c54144769482c5bd751fe72433
Jan Jansen <jan.jansen@gdata.de>
2024-06-13 12:54:19 +0200

    Use env root-dir

--- hack/test-cluster-class.sh
+++ hack/test-cluster-class.sh
@@ -2,27 +2,27 @@
 
 ROOT_DIR=$(cd $(realpath $(dirname $0))/.. && pwd)
 
-if [[ ! -d "./output" ]]; then
+if [[ ! -d "${ROOT_DIR}/output" ]]; then
     echo "create output directory"
-    mkdir "./output"
+    mkdir "${ROOT_DIR}/output"
 fi
 
-if [[ ! -f "./.envfile" ]]; then
+if [[ ! -f "${ROOT_DIR}/.envfile" ]]; then
     echo "create .envfile from envfile.example"
     exit 1
 fi
-source ./.envfile
+source ${ROOT_DIR}/.envfile
 
 echo "Setup Cluster API with ClusterResourceSet and ClusterTopology"
 echo "export EXP_CLUSTER_RESOURCE_SET=\"true\""
 echo "export CLUSTER_TOPOLOGY=\"true\""
 echo "clusterclt init --infrastructure=ionos-cloud"
 
-GENERATED_CLUSTER_CLASS_FILE="./output/generated-clusterclass-template.yaml"
-GENERATED_CLUSTER_FILE="./output/generated-cluster-template-topology-calico.yaml"
+GENERATED_CLUSTER_CLASS_FILE="${ROOT_DIR}/output/generated-clusterclass-template.yaml"
+GENERATED_CLUSTER_FILE="${ROOT_DIR}/output/generated-cluster-template-topology-calico.yaml"
 
-clusterctl generate yaml --from ./templates/clusterclass-template.yaml > $GENERATED_CLUSTER_CLASS_FILE
-clusterctl generate cluster $CLUSTER_NAME -n $NAMESPACE --from ./templates/cluster-template-topology-calico.yaml > $GENERATED_CLUSTER_FILE
+clusterctl generate yaml --from "${ROOT_DIR}/templates/clusterclass-template.yaml" > $GENERATED_CLUSTER_CLASS_FILE
+clusterctl generate cluster $CLUSTER_NAME -n $NAMESPACE --from "${ROOT_DIR}/templates/cluster-template-topology-calico.yaml" > $GENERATED_CLUSTER_FILE
 
 echo "Validate generated resources"
 clusterctl alpha topology plan -f $GENERATED_CLUSTER_CLASS_FILE -f $GENERATED_CLUSTER_FILE -o output/
@@ -34,6 +34,6 @@ echo "kubectl apply -f $GENERATED_CLUSTER_FILE"
 
 echo "Installing calico cni"
 echo "make crs-calico"
-echo "kubectl apply -f ./templates/crs/cni/calico.yaml"
+echo "kubectl apply -f ${ROOT_DIR}/templates/crs/cni/calico.yaml"
 
 echo "Done"
